bf0d6a4075582d48ebfdede6cc3e3a0e332208c4
rand0me <dandydan2k@gmail.com>
2016-08-05 15:47:54 +0300

    Reworked tests with "tape"

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-var path = require('path');
-var Server = require('karma').Server;
+const humanize = require('tap-spec'),
+    tape = require('tape'),
+    path = require('path'),
+    depsMock = require('../mock/Dependencies'),
+    confMock = require('../mock/Configuration'),
+    fs = require('fs');
 
-module.exports = new Server({
-    configFile: path.join(__dirname, 'karma.conf.js'),
-    singleRun: true,
-    autoWatch: true
-});
+
+module.exports = function (INHABIT_CFG) {
+    global.__ark_app__ = { apps: { push: testModule } };
+
+    var Module;
+
+    tape.createStream()
+        .pipe(humanize())
+        .pipe(process.stdout);
+
+    tape('There is an inhabit.cfg.json', function (t) {
+        t.ok(fs.accessSync(INHABIT_CFG) === undefined,  'Accessible: ' + INHABIT_CFG);
+        t.ok(require(INHABIT_CFG).main !== undefined, 'It has "main" property');
+        t.ok(fs.accessSync(path.resolve(require(INHABIT_CFG).main)) === undefined, 'Accessible: ' + path.resolve(require(INHABIT_CFG).main));
+
+        Module = require(path.resolve(require(INHABIT_CFG).main));
+
+        t.end();
+    });
+
+    function testModule(Module) {
+        tape(Module.name + ' is good enough', function (t) {
+            depsMock.then(function (deps) {
+                var module = new Module(confMock, deps);
+
+                t.ok(typeof Module === 'function', Module.name + ' is a constructor');
+                t.ok(typeof module === 'object',   Module.name + ' constructs an object');
+                t.ok(typeof module.getContent === 'function', Module.name + '#getContent method exists');
+
+                var promise = module.getContent();
+
+                t.ok(typeof promise.then === 'function',   Module.name + '#getContent returns promise');
+                t.comment('Waiting for promise resolving');
+                promise.then(function () {
+
+                    t.pass('Promise resolved');
+                    t.equals(typeof module.hasContent(),  'boolean', Module.name + '#hasContent   returns Boolean');
+                    t.equals(typeof module.getTitle(),     'string', Module.name + '#getTitle     returns String');
+                    t.equals(typeof module.getThumbnail(), 'string', Module.name + '#getThumbnail returns String');
+                    t.equals(typeof module.getType(),      'string', Module.name + '#getType      returns String');
+                    t.equals(typeof module.display(),      'string', Module.name + '#display      returns String');
+
+                    t.end();
+                });
+
+                t.timeoutAfter(5000);
+            });
+        });
+    }
+};
---
+++ mock/Dependencies.js
@@ -0,0 +1,42 @@
+var Promise = require('pinkie-promise');
+
+var mockData = [];
+
+var textClassificationService = function () {
+    this.getTaxonomy = function () {
+        return new Promise(function (resolve) { resolve(mockData); });
+    };
+    this.getEntities = function () {
+        return new Promise(function (resolve) { resolve(mockData); });
+    };
+    this.getKeywords = function () {
+        return new Promise(function (resolve) { resolve(mockData); });
+    };
+};
+
+var searchEngineService = function () {
+    this.random = function (provider, transformer) {
+        return new Promise(function (resolve) { resolve(transformer.transform(mockData)); });
+    };
+    this.search = function (provider, transformer) {
+        return new Promise(function (resolve) { resolve(transformer.transform(mockData)); });
+    };
+    this.queryFromTextClassification = function (provider, transformer) {
+        return new Promise(function (resolve) { resolve(transformer.transform(mockData)); });
+    };
+};
+
+module.exports = new Promise(function (resolve, reject) {
+    require('jsdom').env("", function (err, window) {
+        if (err) {
+            reject(err);
+        }
+
+        resolve({
+            $: require('jquery')(window),
+            handlebars: require('handlebars'),
+            textClassificationService: new textClassificationService(),
+            searchEngineService: new searchEngineService()
+        });
+    });
+});
